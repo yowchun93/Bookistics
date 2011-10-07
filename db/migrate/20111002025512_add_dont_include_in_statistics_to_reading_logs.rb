02371d489387b4e98ce0640e28787ada21dc2b44
Francisco Soto <ebobby@gmail.com>
2011-10-07 03:13:36 -0700

    Fixed migration.

--- db/migrate/20111002025512_add_dont_include_in_statistics_to_reading_logs.rb
+++ db/migrate/20111002025512_add_dont_include_in_statistics_to_reading_logs.rb
@@ -1,7 +1,7 @@
 class AddDontIncludeInStatisticsToReadingLogs < ActiveRecord::Migration
   def self.up
     add_column :reading_logs, :use_for_statistics, :boolean, :default => true, :null => false
-    execute 'UPDATE reading_logs SET use_for_statistics = 0 WHERE start_date is not null and start_date = finish_date'
+    execute "UPDATE reading_logs SET use_for_statistics = 'f' WHERE start_date is not null and start_date = finish_date"
   end
 
   def self.down
