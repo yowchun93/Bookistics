3a26ac3285263f6d9f63aed949149f6760d91ae7
Francisco Soto <ebobby@gmail.com>
2011-10-07 02:29:59 -0700

    Statistics are more precise, but may deviate from reality ;)

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -67,33 +67,30 @@ class Statistics
     end
 
     def user_average_books_per_month (user)
-      days_reading = days_between_dates(user_reading_since(user), Date.today)
-      read_books = user_read_books(user)
-
-      return 0.0 if days_reading.to_i == 0 or read_books == 0
-
-      average_per_month(read_books, days_reading)
+      user_average_books_per_year(user) / MONTHS_PER_YEAR
     end
 
     def user_average_books_per_year (user)
-      user_average_books_per_month(user) * MONTHS_PER_YEAR
-    end
-
-    def user_average_pages_per_month (user)
-      days_reading = days_between_dates(user_reading_since(user), Date.today)
-      read_pages = user_read_pages(user)
-
-      return 0.0 if days_reading.to_i == 0 or read_pages == 0
-
-      average_per_month(read_pages, days_reading)
-    end
-
-    def user_average_pages_per_year (user)
-      user_average_pages_per_month(user) * MONTHS_PER_YEAR
+      samples = ReadingLog
+                  .select('start_date, finish_date')
+                  .where("reading_logs.user_id = ?
+                          and start_date is not null
+                          and finish_date is not null
+                          and use_for_statistics = 't'", user.id)
+                  .map { |log| DAYS_PER_YEAR / (log.finish_date - log.start_date).to_f  }
+      samples.inject(:+) / samples.size
     end
 
     def user_average_pages_per_day (user)
       user_average_pages_per_year(user) / DAYS_PER_YEAR
+    end
+
+    def user_average_pages_per_month (user)
+      user_average_pages_per_year(user) / MONTHS_PER_YEAR
+    end
+
+    def user_average_pages_per_year (user)
+      (user_read_pages(user) / user_read_books(user)) * user_average_books_per_year(user)
     end
 
     def user_average_days_per_book (user)
@@ -102,17 +99,6 @@ class Statistics
       return 0.0 if books_per_month == 0.0
 
       DAYS_PER_MONTH / books_per_month
-    end
-
-    private
-
-    def days_between_dates (a, b)
-      return nil if a.class != Date or b.class != Date
-      (a - b).to_i.abs
-    end
-
-    def average_per_month (quantity, days)
-      quantity.to_f / (days.to_f / DAYS_PER_MONTH)
     end
   end
 end
